c0fc24cccedf6a895a4c2499d84d2cd1436eb543
htha9587 <harrison.thacker69@gmail.com>
2016-06-21 17:36:44 -0600

    Version 0.3
    Fully functional address app. Can now delete, edit, and add new persons,
    with alert dialogs if input is incorrect or nonexistent.

--- src/address/MainApp.java
+++ src/address/MainApp.java
@@ -3,6 +3,7 @@ package address;
 import java.io.IOException;
 
 import address.model.Person;
+import address.view.PersonEditDialogController;
 import address.view.PersonOverviewController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -11,6 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainApp extends Application 
@@ -43,80 +45,111 @@ public class MainApp extends Application
 	}
 	
 	/**
-	 * Returns data as ObservableList of Persons.
-	 */
-	public ObservableList<Person> getPersonData()
-	{
-		return personData;
-	}
-	@Override
-	public void start(Stage primaryStage) 
-	{
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("AddressApp");
-		
-		initRootLayout();
-		
-		showPersonOverview();
-	}
+     * Returns the data as an observable list of Persons. 
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
-	/**
-	 * Initializes rootLayout.
-	 */
-	public void initRootLayout()
-	{
-		//Load root Layout from FXML file.
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
-		try {
-			rootLayout = (BorderPane) loader.load();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//Show Scene containing root layout.
-		Scene scene = new Scene(rootLayout);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	
-	/**
-	 * Sets person overview into the center of the root layout.
-	 * @param args
-	 */
-	public void showPersonOverview()
-	{
-		try 
-		{
-			//Load PersonOverview.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
-			AnchorPane personOverview = (AnchorPane) loader.load();
-			
-			//Set Person overview into the center of root layout.
-			rootLayout.setCenter(personOverview);
-			//Give Controller access to MainApp.
-			PersonOverviewController controller = loader.getController();
-			controller.setMainApp(this);
-			 
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Returns main stage.
-	 * @param args
-	 */
-	public Stage getPrimaryStage()
-	{
-		return primaryStage;
-	}
-	
-	public static void main(String[] args) 
-	{
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     * 
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
